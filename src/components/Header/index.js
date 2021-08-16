6e8320cde1d22736a01df85c42af81f0540005f0
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-08-16 00:46:15 -0700

    redirect a href to home page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -19,7 +19,7 @@ function Header(props) {
     <header>
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
       <h2>
-        <a href="/">
+        <a href="/20.-React-Challenge-React-Portfolio/">
           <span role="img" aria-label="camera">
             🙋🏻‍♀️
           </span>
